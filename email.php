ff7e976eb64f51839b7faae65ce595cd9a0d743e
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2020-12-14 11:07:02 +0700

    cap nhat ngay 14/12/2020 luc 11h06

--- email.php
+++ email.php
@@ -1,12 +1,53 @@
 <?php
-    $to = "nhmtam.c3tqcap.a3@gmail.com";
-    $subject = 'Wactchstyle';
-    $message = 'Bạn đã đặt hàng thành công, đơn hàng sẽ vận chuyển đến bạn trong thời gian sớm nhất, cảm ơn bạn!!';
-    $header = "From: watchstyle@gmail.com";
+include   "PHPMailer-6.2.0/src/PHPMailer.php";
+include   "PHPMailer-6.2.0/src/Exception.php";
+include  "PHPMailer-6.2.0/src/OAuth.php";
+include  "PHPMailer-6.2.0/src/POP3.php";
+include  "PHPMailer-6.2.0/src/SMTP.php";
 
-    if(mail($to, $subject, $message, $header)==true){
-        echo 'Đặt hàng thành công!!! => Cảm ơn bạn !!!';
-    }else{
-        echo 'Đặt hàng thất bại';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true); // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = 'nhmtam.c3tqcap.a3@gmail.com'; // SMTP username
+    $mail->Password = 'tamthokha99'; // SMTP password
+    $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465; // TCP port lớn connect to
+    //Recipients
+    $mail->setFrom('watchstyle@gmail.com', 'Mailer');
+    $mail->addAddress('quangtin131299@gmail.com', 'Tâm'); // Add a recipient
+    // $mail->addAddress('ellen@example.com'); // Name is optional
+    // $mail->addReplyTo('quangtin131299@gmail.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); // Optional name
+    //Content
+    // $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'Em yeu anh nhieu lam';
+    $mail->AltBody = 'This is the body in plain text for non-HTML email clients';
+    if ($mail->send()) {
+        echo 'Message has been sent';
     }
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+    // $to = "nhmtam.c3tqcap.a3@gmail.com";
+    // $subject = 'Wactchstyle';
+    // $message = 'Bạn đã đặt hàng thành công, đơn hàng sẽ vận chuyển đến bạn trong thời gian sớm nhất, cảm ơn bạn!!';
+    // $header = "From: watchstyle@gmail.com";
+
+    // if(mail($to, $subject, $message, $header)==true){
+    //     echo 'Đặt hàng thành công!!! => Cảm ơn bạn !!!';
+    // }else{
+    //     echo 'Đặt hàng thất bại';
+    // }
 ?>
